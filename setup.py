a2db04bbc252cc84922d91d8be30ae9d479b2f54
ml14je <49066093+ml14je@users.noreply.github.com>
2020-07-01 15:20:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import setup
 setup(name='Python Personal Package',
       version= '0.1',
       description= 'Personal Python package for the use throughout the NERC-funded PhD in Applied Mathematics at the University of Leeds.',
-      url= 'http://github.com/ml14je/PPP',
+      url= 'http://github.com/ml14je/ppp',
       author= 'Joseph Elmes',
       author_email= 'ml14je@leeds.ac.uk',
       license='None',
       install_requires=[
-          'numpy', 'scipy', 'pandas', 'matplotlib', 'sys', 'os', 'opencv-python', 'pil', 'pytube'
+          'numpy', 'scipy', 'pandas', 'matplotlib', 'opencv-python', 'pillow', 'pytube'
       ],
       zip_safe=False)
